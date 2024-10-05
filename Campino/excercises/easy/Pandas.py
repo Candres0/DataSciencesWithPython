d60fcf74863a81cb4642ac76faae2006006cdb89
Andres Campiño <andrescampino@192.168.0.10>
2024-10-04 21:53:31 -0500

    Practica de Pandas

--- Campino/excercises/easy/Pandas.py
+++ Campino/excercises/easy/Pandas.py
@@ -66,3 +66,15 @@ print(cars.iloc[-2,2])
 # Print sub-DataFrame
 print(cars.loc[['RU','MOR'],['country','drives_right']])
 print(cars.iloc[[4,-2],[1,2]])
+
+# Print out drives_right column as Series
+print(cars.loc[:, 'drives_right'])
+print(cars.iloc[:, 2])
+
+# Print out drives_right column as DataFrame
+print(cars.loc[:,['drives_right']])
+print(cars.iloc[:, [2]])
+
+# Print out cars_per_cap and drives_right as DataFrame
+print(cars.loc[:, ['cars_per_cap', 'drives_right']])
+print(cars.iloc[:, [0,2]])
