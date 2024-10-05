7b29f566607799cecbe2dae9d06e02ecdcb785f8
Hernan Ahumada <hernomo@hotmail.com>
2024-10-04 21:46:07 -0500

    Practica Pandas Hernan

--- Hernan/Pandas.py
+++ Hernan/Pandas.py
@@ -44,3 +44,53 @@ cars = pd.read_csv(r'C:\Users\User\Documents\REPOSITORY\DataSciencesWithPython\F
 
 # Print out cars
 print(cars)
+
+######################################################
+################PANDAS SQUARE BRACKETS################
+######################################################
+
+# Print out country column as Pandas Series
+print(cars['country'])
+
+# Print out country column as Pandas DataFrame
+print(cars[['country']])
+
+# Print out DataFrame with country and drives_right columns
+print(cars[['country','drives_right']])
+
+# Print out first 3 observations
+print(cars[0:3])
+
+# Print out fourth, fifth and sixth observation
+print(cars[3:6])
+
+##########################################
+##############LOC and ILOC################
+##########################################
+# Print out observation for Japan as a serie
+print(cars.loc['JPN'])
+print(cars.iloc[2])
+
+# Print out observations for Australia and Egypt as a DataFrame
+print(cars.loc[['AUS', 'EG']])
+print(cars.iloc[[1,-1]])
+
+# Print out drives_right value of Morocco as a serie
+print(cars.loc['MOR','drives_right'])
+print(cars.iloc[-2,2])
+
+# Print sub-DataFrame
+print(cars.loc[['RU','MOR'],['country','drives_right']])
+print(cars.iloc[[4,-2],[1,2]])
+
+# Print out drives_right column as Series
+print(cars.loc[:, 'drives_right'])
+print(cars.iloc[:, 2])
+
+# Print out drives_right column as DataFrame
+print(cars.loc[:,['drives_right']])
+print(cars.iloc[:, [2]])
+
+# Print out cars_per_cap and drives_right as DataFrame
+print(cars.loc[:, ['cars_per_cap', 'drives_right']])
+print(cars.iloc[:, [0,2]])
