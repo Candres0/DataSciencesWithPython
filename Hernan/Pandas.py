734f8a180c916ea62d283686e96b77b30d28fac9
Hernan Ahumada <hernomo@hotmail.com>
2024-10-10 21:10:55 -0500

    Pandas filtering Hernan

--- Hernan/Pandas.py
+++ Hernan/Pandas.py
@@ -94,3 +94,40 @@ print(cars.iloc[:, [2]])
 # Print out cars_per_cap and drives_right as DataFrame
 print(cars.loc[:, ['cars_per_cap', 'drives_right']])
 print(cars.iloc[:, [0,2]])
+
+#######################################
+##########PANDAS FILTERING#############
+#######################################
+
+import numpy as np
+# Extract drives_right column as Series: dr
+dr = cars["drives_right"]
+
+# Use dr to subset cars: sel
+sel = cars[dr]
+# Convert code to a one-liner
+sel = cars[cars['drives_right']]
+
+# Print sel
+print(sel)
+
+# Create car_maniac: observations that have a cars_per_cap over 500
+cpc = cars["cars_per_cap"]
+many_cars = cpc > 500
+car_maniac = cars[many_cars]
+# Print car_maniac
+print(car_maniac)
+
+#create car_maniac using one line only
+car_maniac = cars[cars["cars_per_cap"]>500]
+# Print car_maniac
+print(car_maniac)
+
+# Create medium: observations with cars_per_cap between 100 and 500
+medium = cars[np.logical_and(cars["cars_per_cap"] > 100, 
+                             cars["cars_per_cap"] < 500
+                            )
+             ]
+
+# Print medium
+print(medium)
